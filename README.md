90d3b9539e43673909d30088c0c63dcdbc8adcf9
Zhang, Zepeng <ZZhang3@StateStreet.com>
2012-12-27 14:22:12 +0800

    New EML!

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
 eml
 ===
 
-Emacs-lisp Markup Language -- convert s-express to xml
+Emacs-lisp Markup Language.
+
+To convert S-expression to XML.
+It can embed Emacs-lisp as script-let like JSP, PHP etc.
+
+For instance:
+```Lisp
+(eml
+ '(html
+   (head
+    (title "Hello EML")
+    (meta (http-equiv . Content-Type)
+          (content . "text/html; charset=UTF-8")))
+   (body
+    (h1 "Hello EML")
+    (p "Hello Joe!")
+    (ul
+     <%
+     (dotimes (i 10)
+       (princ (eml '(li <%= i %>))))
+     %>))))
+```
+It would output below:
+```HTML
+<html>
+  <head>
+    <title>Hello EML</title>
+    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />
+  </head>
+  <body>
+    <h1>Hello EML</h1>
+    <p>Hello Joe!</p>
+    <ul>
+      <li>0</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+    </ul>
+  </body>
+</html>
+```
---
+++ eml.el
@@ -0,0 +1,109 @@
+;;;; eml.el is an acronym Emacs-lisp Markup Language
+;;;;
+;;;; Author: Zhang, Zepeng (Joe) <redraiment@gmail.com>
+;;;; Time-stamp: <2012-12-27 CST>
+;;;; Copyright: (C) 2012 Zhang, Zepeng (Joe)
+;;;;
+;;;; This program is free software; you can redistribute it and/or
+;;;; modify it under the terms of the GNU General Public License as
+;;;; published by the Free Software Foundation; either version 2, or (at
+;;;; your option) any later version.
+;;;;
+;;;; This program is distributed in the hope that it will be useful, but
+;;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;;; General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program ; see the file COPYING.  If not, write to
+;;;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;;;; Boston, MA 02111-1307, USA.
+
+(require 'cl)
+
+;;; Escape
+(defconst eml-escape-characters
+  '(("&" . "&amp;")
+    ("'" . "&apos;")
+    ("\"" . "&quot;")
+    ("<" . "&lt;")
+    (">" . "&gt;")
+    (" " . "&nbsp;"))
+  "List of (from . to) pairs for escaping.")
+
+(defun eml-escape-encode (content)
+  "Escape CONTENT for inclusion in some XML."
+  (reduce
+   #'(lambda (s plist)
+       (replace-regexp-in-string (car plist) (cdr plist) s))
+   eml-escape-characters
+   :initial-value content))
+
+(defun eml-escape-decode (content)
+  "Decode the escape characters in CONTENT."
+  (reduce
+   #'(lambda (s plist)
+       (replace-regexp-in-string (cdr plist) (car plist) s))
+   (reverse eml-escape-characters)
+   :initial-value content))
+
+;;; Attribute
+(defun eml-attribute-p (plist)
+  "Predicate for XML attributs.
+Only Accept (key . value)."
+  (and (consp plist)
+       (atom (car plist))
+       (not (listp (cdr plist)))))
+
+(defun eml-attribute-encode (plist)
+  "Convert a plist to xml style attribute."
+  (format "%s=\"%s\"" (car plist) (or (cdr plist) (car plist) "")))
+
+;;; main
+(defun eml (sexp)
+  "Convert a S-EXP to XML string.
+'(root (person (name . Joe) (skill elisp)))
+=>
+<root><person name=\"Joe\"><skill>elisp</skill></person></root>
+
+S-EXP could contain script-let (`<%' and `%>', `<%=' and `%>')
+followed by a Lisp expression used as part of the content.
+The result of expressions that inside of `<%=' and `%>' will
+insert into content, which the output of expressions that inside
+of `<%' and `%>' will be inserted.
+
+'(root (name <% (princ \"Joe\") %>) (age <%= 23 %>))
+=>
+<root><name>Joe</name><age>23</age></root>"
+  (let ((tag (format "%s" (car sexp)))
+        (attr ())
+        (children ())
+        (value-content? nil)
+        (eval-content? nil))
+    (dolist (e (cdr sexp))
+      (cond
+       ((eq e '%>)
+        (setq value-content? nil
+              eval-content? nil))
+       ((eq e '<%)
+        (setq eval-content? t))
+       ((eq e '<%=)
+        (setq value-content? t))
+       (eval-content?
+        (push (with-output-to-string (eval e)) children))
+       (value-content?
+        (push (format "%s" (eval e)) children))
+       ((eml-attribute-p e)
+        (push (eml-attribute-encode e) attr))
+       ((consp e)
+        (push (eml e) children))
+       (t
+        (push (format "%s" (or e "")) children))))
+    (format "<%s%s%s>"
+            tag
+            (mapconcat (lambda (s) (concat " " s)) (nreverse attr) "")
+            (if children
+              (format ">%s</%s" (apply #'concat (nreverse children)) tag)
+              " /"))))
+
+(provide 'eml)
